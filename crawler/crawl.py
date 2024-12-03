001ca80e704282a006cc0f18b1607d86c1ec1478
zhiyu1998 <542716863@qq.com>
2024-12-03 18:36:49 +0800

    ✨feat: 更新 Crawl 模块，引入BM25内容过滤和Markdown生成策略

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -1,4 +1,6 @@
 from crawl4ai import AsyncWebCrawler, CacheMode, CrawlResult
+from crawl4ai.content_filter_strategy import BM25ContentFilter
+from crawl4ai.markdown_generation_strategy import DefaultMarkdownGenerator
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -9,6 +11,11 @@ async def simple_crawl(url: str) -> CrawlResult:
         result = await crawler.arun(
             url=url,
             cach_mode=CacheMode.ENABLED,
+            markdown_generator=DefaultMarkdownGenerator(
+                content_filter=BM25ContentFilter(user_query=None, bm25_threshold=1.0)
+            ),
+            exclude_external_images=True,
+            excluded_tags=['form', 'nav']
         )
         return result
 
@@ -17,9 +24,13 @@ async def simple_crawl(url: str) -> CrawlResult:
 async def read_root():
     url = request.args.get('url', default=None, type=str)
     crawl_res = await simple_crawl(url)
-    resp = crawl_res.markdown_v2.raw_markdown
+    resp = crawl_res.markdown_v2
+    if resp is None:
+        return {
+            "data": "No data"
+        }
     return {
-        "data": resp
+        "data": resp.raw_markdown
     }
 
 if __name__ == '__main__':
